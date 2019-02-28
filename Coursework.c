d633e8665ef8052936b73917449355a8e8a13952
mcneil1 <36566724+mcneil1@users.noreply.github.com>
2019-02-28 11:34:06 +0000

    Added ability to cycle between players & add moves

--- Coursework.c
+++ Coursework.c
@@ -1,14 +1,89 @@
 #include <stdio.h>
 #include <conio.h>
 
-void newGame();
+void gameBoard();
+
+#define SIZE 10
+char space[SIZE] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
 void main()
 {
-	newGame();
+	int player = 1;
+	int playerWin = 0;
+	int move; 
+	char mark;
+	
+	do
+	{
+
+		gameBoard();
+		
+		player = (player % 2);
+		if (player == 1)
+		{
+			player = 1;
+		}
+		else 
+		{
+			player = 2;
+		}
+		
+		printf("Player %d please enter a number:", player);
+		scanf("%d", &move);
+		
+		
+		if(player == 1)
+		{
+			mark = 'O';
+		}
+		else
+		{
+			mark = 'X';
+		}
+		
+		if (move == 1 && space[0] == '1')
+			space[0] = mark;
+		
+		else if (move == 2 && space[1] == '2')
+			space[1] = mark;
+		
+		else if (move == 3 && space[2] == '3')
+			space[2] = mark;
+		
+		else if (move == 4 && space[3] == '4')
+			space[3] = mark;
+		
+		else if (move == 5 && space[4] == '5')
+			space[4] = mark;
+		
+		else if (move == 6 && space[5] == '6')
+			space[5] = mark;
+		
+		else if (move == 7 && space[6] == '7')
+			space[6] = mark;
+		
+		else if (move == 8 && space[7] == '8')
+			space[7] = mark;
+		
+		else if (move == 9 && space[8] == '9')
+			space[8] = mark;
+		
+		else
+		{
+			printf("Invalid move, try again");
+			player--;
+			getch();
+		}
+
+		
+		player++;
+		
+	} while(playerWin != 1);
+	gameBoard();
+	
 }
 
-void newGame()
+void gameBoard()
 {
 	system("cls");
 	
@@ -16,11 +91,11 @@ void newGame()
 	printf("Player 1 = 'O'   |   Player 2 = 'X'\n\n");
 	
 	printf("           -------------\n");
-	printf("           | 1 | 2 | 3 |\n");
+	printf("           | %c | %c | %c |\n", space[0], space[1], space[2]);
 	printf("           |---|---|---|\n");
-	printf("           | 4 | 5 | 6 |\n");
+	printf("           | %c | %c | %c |\n", space[3], space[4], space[5]);
 	printf("           |---|---|---|\n");
-	printf("           | 7 | 8 | 9 |\n");
+	printf("           | %c | %c | %c |\n", space[6], space[7], space[8]);
 	printf("           -------------\n");
 	
 }
